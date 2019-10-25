c382fc9c25123fd320875261455a94c5df8a6c87
abdelrahman-t <abdurrahman.talaat@gmail.com>
2019-10-25 19:59:31 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='UTF-8') as file:
 
 setup(
     name='falcontyping',
-    version='0.6.0',
+    version='0.7.0',
     url='https://github.com/abdelrahman-t/falcontyping',
     author='Abdurrahman Talaat <abdurrahman.talaat@gmail.com>',
     author_email='abdurrahman.talaat@gmail.com',
